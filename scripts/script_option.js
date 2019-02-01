cd9c49d777d9d1a13cbae7e0f45e3fe3dcb15854
Lupusanghren <brisingr34130@gmail.com>
2019-02-01 13:34:16 +0100

    patern for script bag + new Img

--- scripts/script_option.js
+++ scripts/script_option.js
@@ -4,3 +4,106 @@ window.onload=init();
 function init(){
 	console.log("init");
 }
+
+function requeteMessenger(param,callBack){
+	var httpRequest = new XMLHttpRequest();
+    var url="https://api.twitch.tv/helix/streams?user_login="+param;
+    httpRequest.open("GET", url, true);
+    var key ="";
+    httpRequest.setRequestHeader('Client-ID',key);
+    httpRequest.setRequestHeader("Content-Type", "application/json");
+    httpRequest.addEventListener("load", function () {
+        callBack(httpRequest,nomChaine);
+    });
+    httpRequest.send();
+}
+function requeteFacebook(param,callBack){
+	var httpRequest = new XMLHttpRequest();
+    var url="https://api.twitch.tv/helix/streams?user_login="+param;
+    httpRequest.open("GET", url, true);
+    var key ="";
+    httpRequest.setRequestHeader('Client-ID',key);
+    httpRequest.setRequestHeader("Content-Type", "application/json");
+    httpRequest.addEventListener("load", function () {
+        callBack(httpRequest,nomChaine);
+    });
+    httpRequest.send();
+}
+
+function requeteTwitter(param,callBack){
+	var httpRequest = new XMLHttpRequest();
+    var url="https://api.twitch.tv/helix/streams?user_login="+param;
+    httpRequest.open("GET", url, true);
+    var key ="";
+    httpRequest.setRequestHeader('Client-ID',key);
+    httpRequest.setRequestHeader("Content-Type", "application/json");
+    httpRequest.addEventListener("load", function () {
+        callBack(httpRequest,nomChaine);
+    });
+    httpRequest.send();
+}
+
+function requeteTwitterMessanger(param,callBack){
+	var httpRequest = new XMLHttpRequest();
+    var url="https://api.twitch.tv/helix/streams?user_login="+param;
+    httpRequest.open("GET", url, true);
+    var key ="";
+    httpRequest.setRequestHeader('Client-ID',key);
+    httpRequest.setRequestHeader("Content-Type", "application/json");
+    httpRequest.addEventListener("load", function () {
+        callBack(httpRequest,nomChaine);
+    });
+    httpRequest.send();
+}
+
+function requeteInstagram(param,callBack){
+	var httpRequest = new XMLHttpRequest();
+    var url="https://api.twitch.tv/helix/streams?user_login="+param;
+    httpRequest.open("GET", url, true);
+    var key ="";
+    httpRequest.setRequestHeader('Client-ID',key);
+    httpRequest.setRequestHeader("Content-Type", "application/json");
+    httpRequest.addEventListener("load", function () {
+        callBack(httpRequest,nomChaine);
+    });
+    httpRequest.send();
+}
+
+function requeteInstagramMessaging(param,callBack){
+	var httpRequest = new XMLHttpRequest();
+    var url="https://api.twitch.tv/helix/streams?user_login="+param;
+    httpRequest.open("GET", url, true);
+    var key ="";
+    httpRequest.setRequestHeader('Client-ID',key);
+    httpRequest.setRequestHeader("Content-Type", "application/json");
+    httpRequest.addEventListener("load", function () {
+        callBack(httpRequest,nomChaine);
+    });
+    httpRequest.send();
+}
+
+function requeteWebtoon(param,callBack){
+	var httpRequest = new XMLHttpRequest();
+    var url="https://api.twitch.tv/helix/streams?user_login="+param;
+    httpRequest.open("GET", url, true);
+    var key ="";
+    httpRequest.setRequestHeader('Client-ID',key);
+    httpRequest.setRequestHeader("Content-Type", "application/json");
+    httpRequest.addEventListener("load", function () {
+        callBack(httpRequest,nomChaine);
+    });
+    httpRequest.send();
+}
+
+function requeteSongKick(param,callBack){
+	var httpRequest = new XMLHttpRequest();
+    var url="https://api.twitch.tv/helix/streams?user_login="+param;
+    httpRequest.open("GET", url, true);
+    var key ="";
+    httpRequest.setRequestHeader('Client-ID',key);
+    httpRequest.setRequestHeader("Content-Type", "application/json");
+    httpRequest.addEventListener("load", function () {
+        callBack(httpRequest,nomChaine);
+    });
+    httpRequest.send();
+}
